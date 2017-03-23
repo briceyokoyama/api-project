ff540de352699740be2b3d886712ef7f9cc70450
briceyokoyama <briceyokoyama@gmail.com>
2017-03-23 06:09:43 +0000

    Finished Project

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+app.get('/:id', function (req, res) {
+  if (req.params.id.match(/[a-zA-Z]/)) {
+    var date = new Date(req.params.id)
+  } else {
+    var date = new Date(req.params.id*1000);
+  }
+  var unix = date.getTime()/1000;
+  var natural = months[date.getMonth()] + ' ' + date.getDate().toString() + ', ' + date.getFullYear().toString();
+  var obj = {
+    unix: unix,
+    natural: natural
+  };
+    
+    res.send(obj);
+
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port 8080!');
 })
